107a438386bece5b594f58f6449ea96eef1072e1
Felix Pape <Felix_Pape@ymail.com>
2023-12-02 00:18:07 +0100

    Gekürzt

--- solutions/day1/run.py
+++ solutions/day1/run.py
@@ -3,11 +3,11 @@ import re
 lösung = 0
 test = 'data-01-test'
 live = 'data-01-real'
+
 with open(live, 'r') as datei:
     for zeile in datei:
         zahlen = re.findall(r'\d', zeile)
-        zwischenlösung = 0
-        zwischenlösung += int(zahlen[0])*10
-        zwischenlösung += int(zahlen[-1])
-        lösung += zwischenlösung
+
+        lösung += int(zahlen[0])*10
+        lösung += int(zahlen[-1])
 print(lösung)
